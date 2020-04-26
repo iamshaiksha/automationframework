9c86d2dd5a9495002f488587ea9f4f0171fca9cd
iamshaiksha <iamshaiksha2019@gmail.com>
2020-04-26 14:53:31 +0530

    [shaiksha]add.selenium testscenarios integrated with cucumber tool and genearted json ouput

---
+++ src/main/java/com/bridgelabz/automationframework/runner/RegistrationRunner.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.automationframework.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/main/java/com/bridgelabz/automationframework/features/RegistrationFeature.feature",
+glue="com.bridgelabz.automationframework.RegistrationstepDeff",
+format= {"json:json_output/registeration.json"},
+monochrome=true,
+strict=true,
+dryRun=false)
+public class RegistrationRunner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/main/java/com/bridgelabz/automationframework/runner/Testrunner.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.automationframework.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/main/java/com/bridgelabz/automationframework/features/loginFeatures.feature",
+glue="com.bridgelabz.automationframework.stepDeff",
+format= {"json:json_output/login.json"},
+monochrome=true,
+strict=true,
+dryRun=false)
+public class Testrunner {
+}
